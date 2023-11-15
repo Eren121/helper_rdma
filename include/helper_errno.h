d868a59b54efd9208c4e413500c1a0b78fd7a656
Raphael Frantz <r.r.a.frantz@tue.nl>
2023-11-15 15:12:49 +0100

    Fix

---
+++ include/helper_errno.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <errno.h>
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+
+#ifndef ENSURE
+#define ENSURE(x) if(!(x)) \
+    { const int line = __LINE__; const char *file = __FILE__; \
+      fprintf(stderr, "'" #x "' failed [%s:%d].\n", file, line); exit(EXIT_FAILURE); }
+#endif
+
+/**
+ * If `x` is false, then exit the program with EXIT_FAILURE and print the errno string.
+ */
+#ifndef ENSURE_ERRNO
+#define ENSURE_ERRNO(x) do { if(!(x)) \
+    { const int line = __LINE__; const char *file = __FILE__; \
+      fprintf(stderr, "'" #x "' failed. errno=\"%s\" [%s:%d]\n", strerror(errno), file, line); exit(EXIT_FAILURE); } \
+    } while(0)
+#endif
+
+/**
+ * Exit the program with the error code EXIT_FAILURE.
+ * Print the `info` message and the errno error string.
+ */
+static inline
+void fatal_errno(const char *info)
+{
+    fprintf(stderr, "%s:\n%s\n", info, strerror(errno));
+    exit(EXIT_FAILURE);
+}
+
+static inline
+void fatal_error(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+
+    puts("");
+
+
+    //exit(EXIT_FAILURE);
+
+    // Force segfault
+    *reinterpret_cast<char*>(0) = 0;
+}
