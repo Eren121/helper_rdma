d92c62e10bdc968ffa31803456428e28eeddfe91
Raphaël Frantz <frantze.raphael@gmail.com>
2023-12-29 20:55:06 +0100

    Remove unused files

--- iperf3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Run bandwidth test.
-# Assumes a tmux session inside ghengis with ghengis focused on pane 0 and attila on pane 1.
-
-iperf3 -s 192.168.0.2 &
-tmux send-keys -t 1 'iperf3 -c 192.168.0.2' Enter
-
-fg
